69565e182ebe71a00a96e30dc506163c3bfb05cf
NIxyAz <nicolasms2812@gmail.com>
2026-04-01 19:12:03 -0300

    Update calculadora media.py
    Added calculation for total hours of absence and percentage.

--- calculadora media.py
+++ calculadora media.py
@@ -2,9 +2,10 @@ nota = []
 
 nome = input("Digite o nome do aluno: ")
 nome_da_diciplina = input("Digite o nome da disciplina: ")
-carga_horaria = int(input("Digite a carga horária da disciplina: "))
+carga_horaria_semestral = 300
+print("A carga horária semestral da disciplina é de", carga_horaria_semestral, "horas presenciais.")
 
-#nota 1 
+#nota 1 [0]
 while True:
     nota_1 = float(input("Digite a primeira nota: "))
     if 0 <= nota_1 <= 10:
@@ -13,7 +14,7 @@ while True:
     else:
         print("Nota inválida. Digite uma nota entre 0 e 10.")
 
-#nota 2
+#nota 2 [1]
 while True:
     nota_2 = float(input("Digite a segunda nota: "))
     if 0 <= nota_2 <= 10:
@@ -22,7 +23,7 @@ while True:
     else:
         print("Nota inválida. Digite uma nota entre 0 e 10.")
 
-#nota do PIM
+#nota do PIM [2]
 while True:
     pim = float(input("Digite a nota do PIM: "))
     if 0 <= pim <= 10:
@@ -31,18 +32,20 @@ while True:
     else:
         print("Nota inválida. Digite uma nota entre 0 e 10.")
         
-faltas = int(input("Digite o número de faltas: "))
+faltas = int(input("Digite o número de faltas em horas: "))
 
 media = (nota[0] * 4 + nota[1] * 4 + nota[2] * 2) / 10
 media = round(media, 2) 
+carga_horaria_total = (faltas / carga_horaria_semestral) * 100
 
 print("A média do(a) aluno(a)", nome, "é:", media)
+print("A porcentagem de faltas é:", carga_horaria_total, "%")
 
-if faltas > 30:
+if carga_horaria_total > 25:
     print("Reprovado por faltas")
 elif nota[2] < 5:
     print("Reprovado por pim")
-elif media >= 5 and faltas <= 30:
+elif media >= 5 and carga_horaria_total <= 25:
     print("Aprovado")
 else: 
     print("Reprovado")
